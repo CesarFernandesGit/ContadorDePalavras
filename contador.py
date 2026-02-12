8a2ed1cbee4df2ceaad6ed7b6020abbff7731e30
CesarFernandesGit <cesarfernandesjob@gmail.com>
2026-02-12 14:06:26 -0300

    Removendo o input do arquivo de contador

--- contador.py
+++ contador.py
@@ -1,8 +1,3 @@
-frase = input("Digite uma frase: ")
-palavras = frase.split()
-print(len(palavras))
-print(palavras)
-
 def limpar_texto(texto):
     texto = texto.lower()
     caracteres = ",.!|?;:\"'()[]{}"
@@ -12,5 +7,10 @@ def limpar_texto(texto):
 
 def contar_palavras(frase):
     frase = limpar_texto(frase)
+    if not frase.strip():
+        return {}
     palavras = frase.split()
-    return len(palavras)
+    contagem = {}
+    for palavra in palavras:
+        contagem[palavra] = contagem.get(palavra, 0) + 1
+    return contagem
