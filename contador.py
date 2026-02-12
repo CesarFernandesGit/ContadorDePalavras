9272029796c79c378f4521862afb55d4850c0e29
CesarFernandesGit <cesarfernandesjob@gmail.com>
2026-02-12 13:43:41 -0300

    Realizando o tratamento de erros com a função "limpar texto", evitando caracteres indesejados

--- contador.py
+++ contador.py
@@ -3,6 +3,14 @@ palavras = frase.split()
 print(len(palavras))
 print(palavras)
 
+def limpar_texto(texto):
+    texto = texto.lower()
+    caracteres = ",.!|?;:\"'()[]{}"
+    for char in caracteres:
+        texto = texto.replace(char, "")
+    return texto
+
 def contar_palavras(frase):
+    frase = limpar_texto(frase)
     palavras = frase.split()
     return len(palavras)
