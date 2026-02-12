4a0c233ed7ab77fed7aa089b08130a89a2f7c6b2
CesarFernandesGit <cesarfernandesjob@gmail.com>
2026-02-12 14:07:42 -0300

    Criando um dicionário para contabilizar quantas vezes cada palavra aparece na frase

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from contador import contar_palavras
 
-frase = input("Digite uma frase: ")
-quantidade = contar_palavras(frase)
-print(f"A frase tem {quantidade} palavras")
+frase = input("Digite uma frase: ").strip()
+if not frase:
+    print("Erro: Nenhuma frase foi digitada.")
+else:
+    resultado = contar_palavras(frase)
+    if resultado:
+        print("Contagem de Palavras:")
+        for palavra, quantidade in resultado.items():
+            print(f"{palavra}:{quantidade}")
+    else:
+        print(f"Nenhuma palavra válida foi encontrada.")
